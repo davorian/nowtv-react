cec75139f094e2e7a8148b36e317f46ef0c0f655
Lyndon Fasanya <don.fasanya@gmail.com>
2017-11-16 18:36:13 +0000

    assuming msg.userIds are unique for efficiency

--- src/views/messageList.jsx
+++ src/views/messageList.jsx
@@ -3,14 +3,14 @@ import MessageListItem from "./messageListItem";
 import {sortItemsByDate} from '../utils/sortDates';
 import PropTypes from 'prop-types';
 import { findMemberWithId } from '../selectors/selectors'
-import uuid from 'uuid-v4';
 
 const MessageList = ({messages, members}) => {
   if(!messages) return null;
   const sortedMessages = sortItemsByDate('timestamp', messages);
   const messageListItems = sortedMessages.map((msg) => {
     const member = findMemberWithId(members, msg.userId);
-    return (<MessageListItem  key={uuid()} {...{msg, member}}/>);
+    //assuming msg.userIds are unique for efficiency
+    return (<MessageListItem  key={msg.userId} {...{msg, member}}/>);
   });
   return (<ul>{messageListItems}</ul>)
 };
